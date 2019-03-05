d459ab0d2027eeaf1b1dd7e4e00d36da6c864411
Hidenori Fujimura <hfu@users.noreply.github.com>
2019-03-05 15:21:03 -0500

    update

--- index.js
+++ index.js
@@ -9,9 +9,16 @@ let style = hrequire('hjson/root.hjson')
 
 for (let i in style.layers) {
   if (typeof style.layers[i] === 'string') {
-    style.layers[i] = hrequire(`hjson/${style.layers[i]}.hjson`)
+    const hjsonPath = `hjson/${style.layers[i]}.hjson`
+    if (fs.existsSync(hjsonPath)) {
+      style.layers[i] = hrequire(hjsonPath)
+    } else {
+      console.log(`${hjsonPath} not found. Skip.`)
+      delete style.layers[i]
+    }
   }
 }
+style.layers = style.layers.filter(v => v)
 
 console.log(JSON.stringify(style, null, 2))
 fs.writeFileSync('style.json', JSON.stringify(style, null, 2))
