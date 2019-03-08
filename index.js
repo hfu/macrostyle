9a873a60302f6604615a25245d7dae04c1efb408
Hidenori Fujimura <hfu@users.noreply.github.com>
2019-03-08 16:31:36 -0500

    update

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 const hjson = require('hjson')
 const fs = require('fs')
+const materialColors = require('material-colors')
+const Color = require('color')
 
 const hrequire = (hjsonPath) => {
   return hjson.parse(fs.readFileSync(hjsonPath).toString())
 }
 
-const macroReplace = (v) => {
+let style = hrequire('hjson/root.hjson')
+let macros = hrequire('hjson/macros.hjson')
+
+let colors = {}
+for (const c0 of Object.keys(materialColors)) {
+  if (['white', 'black'].includes(c0)) {
+    colors[c0] = Color(materialColors[c0]).rgb().array().unshift('rgb')
+  } else {
+    for (const c1 of Object.keys(materialColors[c0])) {
+      let v = Color(materialColors[c0][c1]).rgb().array()
+      if (v.length === 3) {
+        v.unshift('rgb')
+      } else {
+        v.unshift('rgba')
+      }
+      colors[`${c0}-${c1}`] = v
+    }
+  }
+}
+
+const replace = (lut, v) => {
   if (typeof v === 'string') {
-    const r = macros[v]
+    const r = lut[v]
     if (r) {
       return r
     } else {
-      throw new Error(`${v} was not found in macros.hjson`)
+      throw new Error(`${v} was not found in ${lut}`)
     }
   } else {
     return v
   }
 }
-
-let style = hrequire('hjson/root.hjson')
-let macros = hrequire('hjson/macros.hjson')
 
 let errorCount = 0
 for (let i in style.layers) {
@@ -27,9 +46,11 @@ for (let i in style.layers) {
     const hjsonPath = `hjson/${style.layers[i]}.hjson`
     if (fs.existsSync(hjsonPath)) {
       style.layers[i] = hrequire(hjsonPath)
-      style.layers[i].filter = macroReplace(style.layers[i].filter)
-      style.layers[i].layout = macroReplace(style.layers[i].layout)
-      style.layers[i].paint = macroReplace(style.layers[i].paint)
+      style.layers[i].filter = replace(macros, style.layers[i].filter)
+      style.layers[i].layout = replace(macros, style.layers[i].layout)
+      style.layers[i].paint = replace(macros, style.layers[i].paint)
+      style.layers[i].paint['fill-color'] =
+        replace(colors, style.layers[i].paint['fill-color'])
     } else {
       errorCount++
       console.log(`${errorCount}: ${hjsonPath} not found. Skip.`)
