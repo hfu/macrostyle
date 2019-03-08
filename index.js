6c337d01bcb9b3a10a353faf30cf243dcdb231d8
Hidenori Fujimura <hfu@users.noreply.github.com>
2019-03-08 11:47:11 -0500

    update

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ const hrequire = (hjsonPath) => {
   return hjson.parse(fs.readFileSync(hjsonPath).toString())
 }
 
+const macroReplace = (v) => {
+  if (typeof v === 'string') {
+    const r = macros[v]
+    if (r) {
+      return r
+    } else {
+      throw new Error(`${v} was not found in macros.hjson`)
+    }
+  } else {
+    return v
+  }
+}
+
 let style = hrequire('hjson/root.hjson')
+let macros = hrequire('hjson/macros.hjson')
 
 let errorCount = 0
 for (let i in style.layers) {
@@ -13,6 +27,9 @@ for (let i in style.layers) {
     const hjsonPath = `hjson/${style.layers[i]}.hjson`
     if (fs.existsSync(hjsonPath)) {
       style.layers[i] = hrequire(hjsonPath)
+      style.layers[i].filter = macroReplace(style.layers[i].filter)
+      style.layers[i].layout = macroReplace(style.layers[i].layout)
+      style.layers[i].paint = macroReplace(style.layers[i].paint)
     } else {
       errorCount++
       console.log(`${errorCount}: ${hjsonPath} not found. Skip.`)
